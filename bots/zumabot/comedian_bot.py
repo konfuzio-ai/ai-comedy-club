9d358583a527b02de7939861ba8e2e45eacfbde7
David Zumaquero <davidzumaquero@Davids-MacBook-Pro.local>
2023-06-22 22:04:24 +0200

    feat(zumabot): add short joke generator

--- bots/zumabot/comedian_bot.py
+++ bots/zumabot/comedian_bot.py
@@ -4,49 +4,54 @@ from transformers import pipeline
 import requests
 import json
 
-url = "https://api.example.com/data"  # Replace with the actual URL you want to request
-
-# Send GET request
-# response = requests.get(url)
-
-# Check if the request was successful (status code 200)
-# if response.status_code == 200:
-# Print the response content
-#   print(response.text)
-# else:
-# print("Request failed with status code:", response.status_code)
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-dataset = "https://datasets-server.huggingface.co/rows?dataset=SocialGrep%2Fone-million-reddit-jokes&config=SocialGrep--one-million-reddit-jokes&split=train&offset=0&limit=100"
+# REDDIT_DATASET is not used, but I would love to use it to train a pretrained LLM
+REDDIT_DATASET = "https://datasets-server.huggingface.co/rows?dataset=SocialGrep%2Fone-million-reddit-jokes&config=SocialGrep--one-million-reddit-jokes&split=train&offset=0&limit=100"
 CHUCK_API_URL = "https://api.chucknorris.io/jokes/random"
 
 
 class ComedianBot:
     def __init__(self):
         self.current_joke: str = ""
-        self.jokes_rating: dict = {}
+        self.jokes_rating: dict = dict()
         self.current_joke_rating: float = 0
         self.current_city: str = "Berlin"
-        self.current_city_context: str = ""
-        self.jokes_learned: list = []
-        self.current_show_jokes: list = []
+        self.jokes: list = list()
+        self.jokes_learned: list = list()
+        self.current_show_jokes: list = list()
         self.dad_joke_generator_pipeline = pipeline('text-generation', model='huggingtweets/dadsaysjokes')
+        self.short_joke_generator = pipeline('text-generation', model='AlekseyKorshuk/gpt2-jokes')
         self.sentiment_pipeline = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+    def get_next_show_city(self, city: str):
+        self.current_city = city
 
     def study_new_jokes(self):
         self._learn_dad_says_jokes()
         self._copy_chuck_norris_jokes()
+        self._learn_short_jokes()
+        random.shuffle(self.jokes_learned)
+        self.jokes += self.jokes_learned
+        self.jokes_learned = list()
+
+    def _learn_short_jokes(self):
+        jokes_generated = self.short_joke_generator("What's", num_return_sequences=4)
+        jokes = [joke['generated_text'] for joke in jokes_generated]
+        self.jokes_learned += jokes
 
     def _learn_dad_says_jokes(self):
         generator = self.dad_joke_generator_pipeline
         prompts = ["My dream is", "I don't like", "I want"]
-        jokes_generated = generator(random.choice(prompts), num_return_sequences=10)
-        jokes = [f"You know when dad says {joke['generated_text']}" for joke in jokes_generated]
+        prompts_current_city = [f"My dream is to live in {self.current_city}", f"I don't like how {self.current_city}",
+                                f"I want {self.current_city}"]
+        jokes_generated = generator(random.choice(prompts), num_return_sequences=8)
+        jokes_generated_with_current_city = random.choice(prompts_current_city)
+        jokes = [f"You know when dad says {joke['generated_text']}" for joke in
+                 jokes_generated + jokes_generated_with_current_city]
         self.jokes_learned += jokes
 
     def _copy_chuck_norris_jokes(self):
 
-        for i in range(10):
+        for _ in range(10):
             response = requests.get(CHUCK_API_URL)
             if response.status_code == 200:
                 joke_data = json.loads(response.text)
@@ -55,19 +60,18 @@ class ComedianBot:
 
     def select_current_show_jokes(self, show_number_of_jokes=20):
 
-        # select 20 jokes from jokes learned taking the last 10 (like if jokes_learned is a LIFO)
-        self.current_show_jokes += self.jokes_learned[-show_number_of_jokes // 2:-1]
+        # select 20 jokes from jokes learned taking the last 10 (like if jokes is a LIFO)
+        last_jokes_slice = slice(-show_number_of_jokes // 2, -1)
+        first_jokes_slice = slice(0, show_number_of_jokes // 2)
+        self.current_show_jokes += self.jokes[last_jokes_slice]
         jokes_rating_sorted = sorted(self.jokes_rating.items(), key=lambda item: item[1])
         jokes_ranking = [joke for joke, rating in jokes_rating_sorted]
-        self.current_show_jokes += jokes_ranking[:show_number_of_jokes // 2]
-
-    def get_city_context(self, city: str):
-        pass
+        self.current_show_jokes += jokes_ranking[first_jokes_slice]
+        random.shuffle(self.current_show_jokes)
 
     def introduce_comedian(self) -> (str, list):
-        introduction = f"Hello everyone, how is people in {self.current_city}"
-        feedback = list()
-        return introduction, feedback
+        introduction = f"Hello everyone, how is people in {self.current_city}!! I'm Zuma and I'm here to make you laught (at least try)"
+        return introduction
 
     def tell_joke(self) -> str:
         joke = random.choice(self.current_show_jokes)
@@ -91,17 +95,41 @@ class ComedianBot:
 
     def add_joke_rating(self):
         self.jokes_rating[self.current_joke] = self.current_joke_rating
+        self.current_joke_rating = 0
 
     def finish_show(self):
-        self.current_show_jokes: list = []
-        self.current_joke_rating: float = 0
-
+        self.current_show_jokes: list = list()
 
 
 if __name__ == "__main__":
     bot = ComedianBot()
     bot.study_new_jokes()
+
+    # Performance simulation 1
     bot.select_current_show_jokes()
-    print(bot.tell_joke())
-    print(bot.current_joke_rating)
-    bot.add_joke_rating()
+
+    bot.introduce_comedian()
+    for i in range(10):
+        print(bot.tell_joke())
+        comment = random.choice(["really good", "bad", "amazing"])
+        bot.notice_feedback(comments=[comment])
+        print(bot.current_joke_rating)
+        bot.add_joke_rating()
+
+    bot.finish_show()
+    print(bot.jokes_rating)
+
+    bot.study_new_jokes()
+    # Performance simulation 2
+    bot.select_current_show_jokes()
+
+    bot.introduce_comedian()
+    for i in range(10):
+        print(bot.tell_joke())
+        comment = random.choice(["Hahahaha good", "no so good", "more or less"])
+        bot.notice_feedback(comments=[comment])
+        print(bot.current_joke_rating)
+        bot.add_joke_rating()
+
+    print("Jokes in both shows")
+    print(set(bot.current_show_jokes) and set(bot.jokes_rating.keys()))
