5f626091e8660f2bd11d31152736fca6022513d5
David Zumaquero <zumaquerodavid@gmail.com>
2023-06-23 15:36:24 +0200

    feat(zumabot): Add first version of the bot that is going to stage

---
+++ bots/zumabot/bot.py
@@ -0,0 +1,31 @@
+from bots.zumabot.comedian_bot import ComedianBot
+from bots.zumabot.judge_bot import JudgeBot
+import random
+
+
+class Bot(ComedianBot, JudgeBot):
+    def __init__(self):
+        ComedianBot.__init__(self)
+        JudgeBot.__init__(self)
+
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.study_new_jokes()
+
+    # Performance simulation 1
+    bot.select_current_show_jokes()
+
+    bot.introduce_comedian()
+    for i in range(10):
+        print(bot.tell_joke())
+        comment = random.choice(["really good", "bad", "amazing"])
+        bot.notice_feedback(comments=[comment])
+        print(bot.current_joke_rating)
+        bot.add_joke_rating()
+
+    print(len(bot.jokes))
+    bot.rate_joke("What's the difference between")
+    print(len(bot.jokes))  # This bot learn jokes when it rates a joke, and it can use it in the future
+
+    bot.finish_show()
