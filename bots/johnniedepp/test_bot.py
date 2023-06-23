e39e5e7194e0ff013e4c71583fb0b608e0874c8d
RHOC <devetherblitz@gmail.com>
2023-06-23 23:51:11 +0200

    "jdepp test_bot.py add positional arg: 'text'"

--- bots/johnniedepp/test_bot.py
+++ bots/johnniedepp/test_bot.py
@@ -5,12 +5,12 @@ from joke_bot import Bot
 def bot():
     return Bot()
 
-def test_tell_joke(bot):
+def test_tell_joke(bot, text):
     joke = bot.tell_joke()
     assert isinstance(joke, str), "Joke is not a string."
     assert len(joke) > 15, "Joke length is not within the correct range."
 
-def test_rate_joke(bot):
+def test_rate_joke(bot, text):
     joke = "Whats wrong with nature? It has too many bugs."
     rating = bot.rate_joke(joke)
     assert isinstance(rating, (int, float)), "Rating is not a number."
