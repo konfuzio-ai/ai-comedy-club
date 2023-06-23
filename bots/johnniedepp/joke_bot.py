aa6174aed3aac78e55e94aeb4dc3c4be83d77e8b
RHOC <devetherblitz@gmail.com>
2023-06-23 23:37:19 +0200

    "jdepp joke_bot.py new spacy sentiment vibe"

--- bots/johnniedepp/joke_bot.py
+++ bots/johnniedepp/joke_bot.py
@@ -1,21 +1,50 @@
 from transformers import pipeline
 from textblob import TextBlob
 import random
+import spacy
+import torch
 
 class Bot:
     name = 'Funny Johnnie D.'
+
     def __init__(self):
-       self.joke_generator = pipeline('text-generation', model='gpt2')
-       self.joke_prefixes = [
-           "My best joke is: "
-       ]
+        self.joke_generator = pipeline('text-generation', model='gpt2')
+        self.joke_prefixes = [
+            "My best joke is: "
+        ]
+
+        # Initialize the NLP components
+        self.sentiment_analyzer = TextBlob()
+        self.nlp = spacy.load("en_core_web_sm")
 
     def tell_joke(self):
+        # Define the joke attributes
+        humor = "funny"
+        creativity = "high"
+        timeliness = "current"
+        audience = "everyone"
+        comedian = "Johnnie Depp"
+
         # Use the GPT-2 model to generate a joke
         # Choose a random prefix for the joke
         prefix = random.choice(self.joke_prefixes)
-        joke = self.joke_generator(f'{prefix}', max_length=75, do_sample=True)[0]['generated_text']
-        return joke
+        generated_joke = self.joke_generator(f'{prefix}', max_length=75, do_sample=True)[0]['generated_text']
+
+        # Analyze the sentiment of the joke
+        sentiment = self.analyze_sentiment(generated_joke)
+
+        # Return the formatted joke
+        formatted_joke = f"Tell a joke that is {humor}, {creativity}, {timeliness} for {audience} in the style of {comedian}:"
+        formatted_joke += f"\n{generated_joke}"
+        formatted_joke += f"\nSentiment: {sentiment}"
+        return formatted_joke
+
+    def analyze_sentiment(self, text):
+        # Use TextBlob for sentiment analysis
+        blob = TextBlob(text)
+        polarity = blob.sentiment.polarity
+        sentiment = "Positive" if polarity > 0 else "Negative" if polarity < 0 else "Neutral"
+        return sentiment
 
     def rate_joke(self, joke):
         # Rate the joke based on its sentiment polarity
