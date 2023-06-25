2d6ec23ebb3ea678a962f6bb75f8bf8b7ad99fc5
Guja <g.mekokishvili@omedia.ge>
2023-06-26 00:54:03 +0400

    docs: document BaseBot methods

--- interfaces/base_bot_interface.py
+++ interfaces/base_bot_interface.py
@@ -6,8 +6,15 @@ class BaseBot(ABC):
 
     @abstractmethod
     def tell_joke(self) -> str:
+        """
+        This method should generate and return a string containing a joke.
+        """
         pass
 
     @abstractmethod
     def rate_joke(self, joke: str) -> int:
+        """
+        This method should take a string (representing the joke to be rated) and return an integer from 1 to 10, which
+        represents the rating of the joke.
+        """
         pass
