0ade031094669e5c929b0e969a0a6e3a1d95469d
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2023-09-10 16:48:25 +0200

    minor test rate

--- bots/xinyue_joke/test_bot.py
+++ bots/xinyue_joke/test_bot.py
@@ -12,4 +12,4 @@ def test_tell_joke(bot):
 def test_rate_joke(bot):
     joke = "What do you call fake spaghetti? An impasta!"
     rating = bot.rate_joke(joke)
-    assert isinstance(rating, (int, float)), "Rating is not a number." 
+    assert isinstance(rating, int), "Rating is not a number." 
