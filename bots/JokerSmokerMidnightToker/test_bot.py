526d0a6c84c2f64e8abe67211774c063552e75f3
Bleron Limani <bleroni@gmail.com>
2023-10-24 14:51:07 +0200

    Minor test change

--- bots/JokerSmokerMidnightToker/test_bot.py
+++ bots/JokerSmokerMidnightToker/test_bot.py
@@ -1,30 +1,27 @@
 import pytest
 from .joke_bot import Bot
 
-# def test_tell_joke():
-#     bot = Bot()
-#     main_context = "tell me a joke about the country that is the current winner of men's football world cup"
-#     joke_reply = bot.tell_joke(main_context)
-#     assert len(joke_reply) > 10
-#     assert isinstance(joke_reply, str)
-#     assert isinstance(bot.verify_context(main_context, joke_reply), bool)
-
-# def test_tell_joke_with_memory():
-#     bot = Bot()
-#     main_context = "tell me a joke about birds and trees"
-#     joke_reply_1 = bot.tell_joke(main_context)
-#     joke_reply_2 = bot.tell_joke("tell me another one")    
-#     assert len(joke_reply_2) > 10
-#     assert isinstance(joke_reply_2, str)
-#     assert isinstance(bot.verify_context(main_context, joke_reply_2), bool)
-
-# def test_rate_joke():
-#     bot = Bot()
-#     rating = bot.rate_joke("Why didn't Argentina bring a map to the World Cup? Because they already knew the way to the final!")
-#     assert isinstance(rating, int)
-#     assert rating >= 1 and rating <= 10
-
-def test_dummy_joke():
+def test_tell_joke():
     bot = Bot()
-    assert isinstance(bot.verify_context('dummy1', 'dummy2'), bool)    
+    main_context = "tell me a joke about the country that is the current winner of men's football world cup"
+    joke_reply = bot.tell_joke(main_context)
+    assert len(joke_reply) > 10
+    assert isinstance(joke_reply, str)
+    assert isinstance(bot.verify_context(main_context, joke_reply), bool)
+
+def test_tell_joke_with_memory():
+    bot = Bot()
+    main_context = "tell me a joke about birds and trees"
+    joke_reply_1 = bot.tell_joke(main_context)
+    joke_reply_2 = bot.tell_joke("tell me another one")    
+    assert len(joke_reply_2) > 10
+    assert isinstance(joke_reply_2, str)
+    assert isinstance(bot.verify_context(main_context, joke_reply_2), bool)
+
+def test_rate_joke():
+    bot = Bot()
+    rating = bot.rate_joke("Why didn't Argentina bring a map to the World Cup? Because they already knew the way to the final!")
+    assert isinstance(rating, int)
+    assert rating >= 1 and rating <= 10
+
 
