94d8eb6d493deb72ab5c9e1909275f355e024e2a
sarahelmasryy <53651120+sarahelmasryy@users.noreply.github.com>
2023-11-13 19:04:52 +0200

    Delete bots directory

--- bots/tizianococcio/src/gptjokerater.py
+++
@@ -1,6 +0,0 @@
-from src.rater import JokeRaterInterface
-
-class GPTJokeRater(JokeRaterInterface):
-    def rate_joke(self, joke):
-        # For demonstration purposes, this returns a constant
-        return 2
